e2300c955c35b77d1a5b634e2f00bb0e80729807
Sangza <ucheokenyidm@gmail.com>
2024-10-02 20:52:01 +0100

    minor commit

--- database/models.js
+++ database/models.js
@@ -1,13 +1,80 @@
-import { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const UserSchema = new Schema({
-
+const UserSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required:true
+    },
+    address:String,
+    following:[{
+         address:String,
+    }],
+    followers:[{
+        address:String,
+    }],
+    evenCreated:[mongoose.Schema.Types.ObjectId],
+    eventJoined:[mongoose.Schema.Types.ObjectId],
+    photos:[mongoose.Schema.Types.ObjectId],
+    albumCreated:[mongoose.Schema.Types.ObjectId],
+    albumJoined:[mongoose.Schema.Types.ObjectId],
 });
 
-const EventSchema = new Schema({
-
+const EventSchema = new mongoose.Schema({
+   photo:String,
+   title:String,
+   description:String,
+   locationType:{
+    type:String,
+    enum:['venue','online','to be announced']
+   },
+   location:String,
+   eventType:{
+    type:String,
+    enum:['single','recurring event']
+   },
+   eventDate:{
+    type:Date
+   },
+   startEventtime:Tim
 });
 
-export const UserModel = Model("User", UserSchema);
 
-export const EventModel = Model("Event", EventSchema);
+const TicketSchema = new mongoose.Schema({
+     ticketType:{
+        type:String,
+        required:true,
+        enum:['free','paid','donation']
+     },
+     name:String,
+     available:{
+        type:Number,
+        required:true,
+     },
+     price:{
+        type:Number,
+        required:true
+     },
+     ticketSalesEnds:Date,
+     tickSaleOut:{
+        type:String,
+        enum:['before','after']
+     },
+     startTime:String,
+     endTime:String,
+     host:[
+        {
+           host:String
+        }
+     ],
+     artist:[
+        {
+           artist:String
+        }
+     ]
+});
+
+export const UserModel = mongoose.Model("User", UserSchema);
+
+export const EventModel = mongoose.Model("Event", EventSchema);
+
+export const TicketModel = mongoose.Model("Ticket", TicketSchema);
